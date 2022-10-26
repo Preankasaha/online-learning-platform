8520c017d07830fd6bb81d365e3fe4d66da026bc
unknown <preansaha@gmail.com>
2022-10-26 11:04:13 +0600

    registration functionctinality done

--- src/pages/Register&LogIn/Registration/Registration.js
+++ src/pages/Register&LogIn/Registration/Registration.js
@@ -15,58 +15,65 @@ const Registration = () => {
         const password = form.password.value;
         console.log(name, photoURL, email, password)
 
-       
+        //registration
+        createUser(email, password)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
 
-        return (
-            <div>
-                <div className="hero min-h-screen bg-base-200">
-                    <div className="hero-content flex-col">
-                        <div className="text-center lg:text-left">
-                            <h1 className="text-5xl font-bold">Please Register Now!</h1>
-                            <p className="py-6">If you are new to this site, please register first</p>
-                        </div>
-                        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                            <form onSubmit={handleSubmit} className="card-body">
-                                <div className="form-control">
-                                    <label className="label">
-                                        <span className="label-text">Name</span>
-                                    </label>
-                                    <input type="text" name='name' placeholder="userName" className="input input-bordered" />
-                                </div>
-                                <div className="form-control">
-                                    <label className="label">
-                                        <span className="label-text">PhotoURL</span>
-                                    </label>
-                                    <input type="text" name='photoURL' placeholder="Enter your photoURL" className="input input-bordered" />
-                                </div>
-                                <div className="form-control">
-                                    <label className="label">
-                                        <span className="label-text">Email</span>
-                                    </label>
-                                    <input type="email" name='email' placeholder="email" className="input input-bordered" required />
-                                </div>
-                                <div className="form-control">
-                                    <label className="label">
-                                        <span className="label-text" name='password'>Password</span>
-                                    </label>
-                                    <input type="password" name='password' placeholder="password" className="input input-bordered" required />
-                                    <label className="label">
+            })
+            .catch(error => console.error(error))
+    }
 
-                                        <Link to='/login' className="label-text-alt link link-hover">Already have an account? Sign In</Link>
-                                    </label>
-                                </div>
-                                <div className="form-control mt-6">
-                                    <button className="btn btn-primary">Register</button>
-                                </div>
-                            </form>
-                        
-                            <button className="btn btn-outline btn-success">Sign In With Google</button>
-                        </div>
+    return (
+        <div>
+            <div className="hero min-h-screen bg-base-200">
+                <div className="hero-content flex-col">
+                    <div className="text-center lg:text-left">
+                        <h1 className="text-5xl font-bold">Please Register Now!</h1>
+                        <p className="py-6">If you are new to this site, please register first</p>
+                    </div>
+                    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                        <form onSubmit={handleSubmit} className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Name</span>
+                                </label>
+                                <input type="text" name='name' placeholder="userName" className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">PhotoURL</span>
+                                </label>
+                                <input type="text" name='photoURL' placeholder="Enter your photoURL" className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="email" name='email' placeholder="email" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text" name='password'>Password</span>
+                                </label>
+                                <input type="password" name='password' placeholder="password" className="input input-bordered" required />
+                                <label className="label">
+
+                                    <Link to='/login' className="label-text-alt link link-hover">Already have an account? Sign In</Link>
+                                </label>
+                            </div>
+                            <div className="form-control mt-6">
+                                <button className="btn btn-primary">Register</button>
+                            </div>
+                        </form>
+                        {/* onClick={handleGoogleSignIn} */}
+                        <button className="btn btn-outline btn-success">Sign In With Google</button>
                     </div>
                 </div>
             </div>
-        );
-    };
-}
+        </div>
+    );
+};
 
 export default Registration;
