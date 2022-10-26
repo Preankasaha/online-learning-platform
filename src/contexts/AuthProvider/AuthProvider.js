5bfafeff75bfb6fe8431bf643bb7c7a00aa0fc7d
unknown <preansaha@gmail.com>
2022-10-26 11:17:10 +0600

    log in page set up done

--- src/contexts/AuthProvider/AuthProvider.js
+++ src/contexts/AuthProvider/AuthProvider.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import { createContext } from 'react';
-import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth';
+import { createUserWithEmailAndPassword, getAuth} from 'firebase/auth';
 import app from '../../firebase/firebase.config';
 
 export const AuthContext = createContext();
 const auth = getAuth(app);
 const AuthProvider = ({ children }) => {
-
+    
     const user = { displayname: 'ami' };
 
     const createUser = (email, password) => {
         return createUserWithEmailAndPassword(auth, email, password)
     }
-
-    const authInfo = { user, createUser };
+    
+    const authInfo = { user, createUser};
     return (
         <div>
             <AuthContext.Provider value={authInfo}>
--- src/pages/Register&LogIn/LogIn/LogIn.js
+++ src/pages/Register&LogIn/LogIn/LogIn.js
@@ -1,12 +1,50 @@
 import React, { useContext } from 'react';
-
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../../../contexts/AuthProvider/AuthProvider';
 
 const LogIn = () => {
+    const { user} = useContext(AuthContext);
 
+    const handleSubmit = event => {
+        event.preventDefault();
+        const form = event.target;
+        console.log(form);
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password);
+    }
 
     return (
-        <div>
-            log
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col">
+                <div className="text-center lg:text-left">
+                    <h1 className="text-5xl font-bold">Please Login now!</h1>
+                    <p className="py-6">Provident cupiditate voluptatem et in.</p>
+                </div>
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <form onSubmit={handleSubmit} className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" name='email' placeholder="email" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text" name='password'>Password</span>
+                            </label>
+                            <input type="password" name='password' placeholder="password" className="input input-bordered" required />
+                            <label className="label">
+                                <Link href="#" className="label-text-alt link link-hover">Forgot password?</Link>
+                            </label>
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-primary">Sign In</button>
+                        </div>
+                    </form>
+
+                </div>
+            </div>
         </div>
 
     );
